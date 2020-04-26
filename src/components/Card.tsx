09b5bdada7fbc56aae4325d934455b02747fd94a
win11905 <donothavemoney@gmail.com>
2020-04-26 19:45:46 +0700

    :sparkles: Use webp instead of jpeg

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import { Box } from '@chakra-ui/core'
+import { Box, PseudoBox } from '@chakra-ui/core'
+
+const boxConfig = {
+  boxShadow: 'md',
+  rounded: 'lg',
+  _hover: { boxShadow: 'xl' },
+  transition: '0.2s',
+}
 
 export const Card = (props) => {
-  if (props.img) {
-    return (
-      <Box boxShadow="lg" borderRadius={10}>
-        <React.Fragment>
-          <img src={props.img}></img>
+  return (
+    <React.Fragment>
+      {props.img ? (
+        <PseudoBox {...boxConfig}>
+          <picture>
+            <source type="image/webp" srcSet={`/${props.img}.webp`} />
+            <source type="image/jpeg" srcSet={`/${props.img}.jpg`} />
+            <img src={`/${props.img}.jpg`} alt="" />
+          </picture>
           <Box p={5}>{props.children}</Box>
-        </React.Fragment>
-      </Box>
-    )
-  } else {
-    return (
-      <Box p={5} boxShadow="lg" borderRadius={10}>
-        {props.children}
-      </Box>
-    )
-  }
+        </PseudoBox>
+      ) : (
+        <PseudoBox p={5} {...boxConfig}>
+          {props.children}
+        </PseudoBox>
+      )}
+    </React.Fragment>
+  )
 }
--- src/constants/educations.ts
+++ src/constants/educations.ts
@@ -9,14 +9,14 @@ export const educations: IEducations[] = [
   {
     name: 'Triam Udom Suksa School',
     year: '2017-2020',
-    img: '/tu.jpg',
+    img: 'tu',
     desc:
       'Founded in 1938 as a preparatory school for Chulalongkorn University, Triam Udom Suksa School is a state school in Central Bangkok, Thailand. It admits upper-secondary students (mathayom 4–6, equivalent to grades 10–12) and has the largest yearly enrollment in the country.',
   },
   {
     name: 'Assumption Samutprakarn School',
     year: '2008-2017',
-    img: '/acsp.jpg',
+    img: 'acsp',
     desc:
       'Assumption Samutprakarn Schhol is a private Catholic school in Samut Prakarn, Thailand. Founded and run by the Brothers of St. Gabriel since 1979 as the thirteenth educational institution of the congregation, the school provides education to students from grades 1 to 12.',
   },
